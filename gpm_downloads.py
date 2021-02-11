84ab0c80c399526c9f17e611c164e36ff5cfecc2
Ingrid Ingemarsson <inina@hotmail.com>
2021-02-11 17:16:54 +0100

    solved same resolution issue and added ignoring of known warnings

--- gpm_downloads.py
+++ gpm_downloads.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from funs_gpm_downloads import *
 from pansat.products.satellite.gpm import l2b_gpm_cmb
 from pansat.products.satellite.goes import GOES16L1BRadiances
@@ -10,11 +9,16 @@ import xarray as xr
 from pyresample import load_area
 import yaml
 import numpy as np
+from datetime import datetime
+import time
+import warnings
 
 ############################### CONSTANTS ###############################
 
+start_timing = time.time()
+
 # GOES channels to download
-channels = [1,2] #[8, 13]
+channels = [8,13] #[8, 13]
 
 # Paths
 linkfile = "link_file_test.txt" #"2017-12-18--2018-12-17-with-shapefile.txt" 
@@ -45,9 +49,11 @@ link_file = open(linkfile, "r") #File with all linkes from the Earth data search
 link_list = link_file.readlines()
 link_file.close()
 
-# ----------------------------- FOR EACH LINK -----------------------------
-for i in range(0,2,2): 
 
+for i in range(0,2,8): 
+	
+	# ----------------------------- FOR EACH LINK -----------------------------
+	
 	print(i)
 	gpm_file_t_0, gpm_file_t_1 = gpm_extract_datetime(link_list[i])
 	print('GPM time: ' + str(gpm_file_t_0) + ', ' + str(gpm_file_t_1))
@@ -80,68 +86,77 @@ for i in range(0,2,2):
 		timediff = [np.abs((time_goes-mean_time_brazil).seconds) 
 		for time_goes in time_list_goes]				
 		goes_index = np.argmin(timediff)
-		
-	#TODO: if files_tmp is empty?
-			
+	elif (len(files_tmp) == 0):
+		continue    
+	
 	files_goes.append(files_tmp[goes_index])
 		
 		
 	for channel in channels[1:]:
-		files_tmp = GOES16L1BRadiances("F", channel).download(in_brazil_t_0, in_brazil_t_1, path_to_download_storage_goes)
+		files_tmp = GOES16L1BRadiances("F", channel).download(in_brazil_t_0, 
+		in_brazil_t_1, path_to_download_storage_goes)
 		files_goes.append(files_tmp[goes_index])
 		
-		
-	print(files_goes)
-
+	print('goes files downloaded')	
+	
 	# TEST
 	#files_08 = [Path('GOES-16/OR_ABI-L1b-RadF-M3C08_G16_s20180732000427_e20180732011193_c20180732011242.nc')] #TEST
 	#files_13 = [Path('GOES-16/OR_ABI-L1b-RadF-M3C13_G16_s20180732000427_e20180732011205_c20180732011259.nc')] #TEST
 	#files_goes = [files_08[0], files_13[0]] #TEST
-
-		
 
 	# Resample coordinates for GPM data into GOES data format
 	swath_def = geometry.SwathDefinition(lons=dataset_gpm['longitude'], lats=dataset_gpm['latitude'])
 	gpm_transformed_data = kd_tree.resample_nearest(swath_def, dataset_gpm['surface_precipitation'].values, 
 	area_def, radius_of_influence=50000, epsilon=0.5, fill_value=None)
 	print('gpm transformed')
+	
 
 	files_goes = map(str, files_goes)
 	goes_scn = Scene(reader='abi_l1b', filenames=files_goes)
 	av_dat_names = goes_scn.available_dataset_names()
-	goes_scn.load((av_dat_names))
-	print(av_dat_names)
-	goes_scn = goes_scn.resample(goes_scn.min_area(), resampler = 'native')
-	#print(goes_scn.min_area())
-	print(goes_scn)
-	#TODO: fix resampling resolution
-	ref_height = goes_scn[channel[0]].y.shape[0]
-	ref_width = goes_scn[channel[0]].x.shape[0]
-	goes_scn = goes_scn.aggregate(x=np.int(ref_width/width), y=np.int(ref_height/height), func='mean') # yields x = np.divide(x1, x2, out) error,mean of nan values?
-	print(goes_scn)
+
+	# This is a warning regarding loss of projection information when converting to a PROJ string
+	with warnings.catch_warnings():
+		warnings.simplefilter('ignore')
+		goes_scn.load((av_dat_names))
 	
-	'''
+	print('Channels: ' +str(av_dat_names))
+	
+	# This is a warning regarding loss of projection information when converting to a PROJ string
+	with warnings.catch_warnings():
+		warnings.simplefilter('ignore')	
+		goes_scn = goes_scn.resample(goes_scn.min_area(), resampler = 'native')
+		
+	ref_height = goes_scn['C'+str(channels[0]).zfill(2)].y.shape[0]
+	ref_width = goes_scn['C'+str(channels[0]).zfill(2)].x.shape[0]
+	
+	goes_scn = goes_scn.aggregate(x=np.int(ref_width/width), 
+	y=np.int(ref_height/height), func='mean')
+	
+	# RuntimeWarning: invalid value encountered in true_divide x = np.divide(x1, x2, out)
+	# Caused by doing mean of nan-values in aggregate
+	with warnings.catch_warnings():
+		warnings.simplefilter('ignore')
+		# CREATE combined dataset (full disk)
+		keys = ['C'+str(i).zfill(2) for i in channels]
+		values = [(["y", "x"], goes_scn['C' +str(i).zfill(2)].values) for i in channels]
+		keys.append('gpm_precipitation')
+		values.append((["y", "x"], gpm_transformed_data))
+		data_vars_dict = dict(zip(keys, values))
 
-	# CREATE combined dataset (full disk)
-	keys = ['C'+str(i).zfill(2) for i in channels]
-	values = [(["y", "x"], goes_scn['C' +str(i).zfill(2)].values) for i in channels]
-	keys.append('gpm_precipitation')
-	values.append((["y", "x"], gpm_transformed_data))
-	data_vars_dict = dict(zip(keys, values))
-
-	dataset = xr.Dataset(
-		coords = dict(
-			y = (["y"], goes_scn['C'+str(channels[0]).zfill(2)].y), 
-			x = (["x"], goes_scn['C'+str(channels[0]).zfill(2)].x)),
-		data_vars = data_vars_dict, 
-		attrs = dict(
-			time_goes = str({'start_time': goes_scn['C'+str(channels[0]).zfill(2)].attrs['start_time'],
-				'end_time': goes_scn['C'+str(channels[0]).zfill(2)].attrs['end_time']}), 
-			time_gpm = str({'start_time': gpm_file_t_0, 'end_time': gpm_file_t_1, 
-				'approx_start_time_region': in_brazil_t_0, 'approx_end_time_region': in_brazil_t_1})
-				))
+		dataset = xr.Dataset(
+			coords = dict(
+				y = (["y"], goes_scn['C'+str(channels[0]).zfill(2)].y), 
+				x = (["x"], goes_scn['C'+str(channels[0]).zfill(2)].x)),
+			data_vars = data_vars_dict, 
+			attrs = dict(
+				time_goes = str({'start_time': goes_scn['C'+str(channels[0]).zfill(2)].attrs['start_time'],
+					'end_time': goes_scn['C'+str(channels[0]).zfill(2)].attrs['end_time']}), 
+				time_gpm = str({'start_time': gpm_file_t_0, 'end_time': gpm_file_t_1, 
+					'approx_start_time_region': in_brazil_t_0, 'approx_end_time_region': in_brazil_t_1})
+					))
 	print('dataset created')
-
+	
 	# CROPPING dataset to interesting region
 	mask_x = (dataset.x > region_corners[0])*(dataset.x < region_corners[2])
 	mask_y = (dataset.y > region_corners[1])*(dataset.y < region_corners[3])
@@ -150,17 +165,14 @@ for i in range(0,2,2):
 	print('dataset cropped')
 
 	# Writing dataset to file
-	filename = path_to_store_processed_data+'/datasetS'+str(gpm_file_t_0).replace(" ", "")+'E'+str(gpm_file_t_1).replace(" ", "")+'.nc'
+	filename = path_to_store_processed_data+'/datasetS'+str(gpm_file_t_0).replace(" ", "")+'E'+str(gpm_file_t_1).replace(" ", "")+str(channels)+'created'+str(datetime.now()).replace(" ", "")+'.nc'
 	sliced_dataset.to_netcdf(filename)
 	
 	print('dataset saved')
 	
 	# -------------------------------------------------------------------------
-
-	'''
-
-
-
-
-
+	
+end_timing = time.time()
+total_timing = end_timing-start_timing
+print('total time: ' + str(total_timing)+ ' seconds')
 
