e50296e047723a007809f9ba2b463dc5873cd98d
Ingrid Ingemarsson <inina@hotmail.com>
2021-02-12 18:32:15 +0100

    changed to creation of smaller images

--- funs_gpm_downloads.py
+++ funs_gpm_downloads.py
@@ -5,6 +5,9 @@ import numpy as np
 import cartopy
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
+import xarray as xr
+from pansat.products.satellite.gpm import l2b_gpm_cmb
+from pyresample import kd_tree, geometry
 
 # Extracts date from resulting link in the earth data search
 def gpm_extract_datetime(mystr):
@@ -59,6 +62,74 @@ def region_plot(area_def, dataset, feature):
 	ax.add_feature(cartopy.feature.BORDERS)
 	plt.imshow(dataset[feature], transform=crs, extent=crs.bounds, origin='upper')
 	plt.show()
+	
+def create_box_dataset(box_num, gpm_transformed_data, region_center_y_idy, projvec, files_gpm, number_of_pixels):
+		box_idy_up = region_center_y_idy+int((box_num+1)*number_of_pixels)
+		box_idy_do = region_center_y_idy+int(box_num*number_of_pixels)
+
+		swath_idx_up = np.where(np.isnan(gpm_transformed_data[box_idy_up,:,0]) == False)[0]
+		swath_idx_do = np.where(np.isnan(gpm_transformed_data[box_idy_do,:,0]) == False)[0]
+
+		box_middle_x = int(np.mean([min(min(swath_idx_up), min(swath_idx_do)), max(max(swath_idx_up), max(swath_idx_do))]))
+		box_idx_le = box_middle_x-int(0.5*number_of_pixels)
+		box_idx_ri = box_middle_x+int(0.5*number_of_pixels)
+
+
+		#plt.imshow(gpm_transformed_data[box_idy_do:box_idy_up, box_idx_le:box_idx_ri,0])
+		#plt.show()
+		box_data = gpm_transformed_data[box_idy_do:box_idy_up, box_idx_le:box_idx_ri,0]
+		box_t_0 = np.nanmin(gpm_transformed_data[box_idy_do:box_idy_up, box_idx_le:box_idx_ri,1]).astype('datetime64[ns]')
+		box_t_1 = np.nanmax(gpm_transformed_data[box_idy_do:box_idy_up, box_idx_le:box_idx_ri,1]).astype('datetime64[ns]')
+
+		projcoords_x, projcoords_y = projvec
+		box_area_extent = [projcoords_x[box_idx_le], projcoords_y[box_idy_up], projcoords_x[box_idx_ri], projcoords_y[box_idy_do]]
+
+		dataset = xr.Dataset(
+				coords = dict(
+					y = (["y"], projcoords_y[box_idy_do:box_idy_up]), 
+					x = (["x"], projcoords_x[box_idx_le:box_idx_ri])),
+				data_vars = dict(gpm_precipitation = (["y","x"], box_data)), 
+				attrs = dict(
+					area_extent = box_area_extent,
+					shape = [number_of_pixels,number_of_pixels],
+					time_in = box_t_0, time_out = box_t_1,
+					filename = files_gpm[0]
+						))
+						
+		return(dataset)
+		
+def gpm_data_processing(number_of_pixels, gpm_file_time, path_to_download_storage_gpm, area_def, region_corners):
+
+	'''
+	# DOWNLOAD gpm data
+	files_gpm = l2b_gpm_cmb.download(gpm_file_time[0], gpm_file_time[1], path_to_download_storage_gpm)
+	dataset_gpm = l2b_gpm_cmb.open(files_gpm[0])
+	'''
+	# TEST
+	files_gpm = ["GPM/2B.GPM.DPRGMI.2HCSHv4-1.20180314-S194719-E211954.022965.V06A.HDF5"]
+	dataset_gpm = l2b_gpm_cmb.open(files_gpm[0]) #TEST
+
+	# Resample coordinates for GPM data into GOES data format
+	repeted_time = np.transpose(np.tile(dataset_gpm['scan_time'].astype(np.int),(49,1)))
+	gpm_data = np.stack((dataset_gpm['surface_precipitation'].values, repeted_time), axis = 2)
+	swath_def = geometry.SwathDefinition(lons=dataset_gpm['longitude'], lats=dataset_gpm['latitude'])
+	gpm_transformed_data = kd_tree.resample_nearest(swath_def, gpm_data, 
+	area_def, radius_of_influence=3600, epsilon=0, fill_value=np.nan)
+
+
+	region_center_y = np.mean([region_corners[1],region_corners[3]])
+	projvec = area_def.get_proj_vectors() #tuple (X,Y)
+	projcoords_y = projvec[1]
+	projcoords_x = projvec[0]
+	projcoords_y_diff = np.abs(projcoords_y - region_center_y)
+	region_center_y_idy = np.argmin(projcoords_y_diff)
+	
+	#TODO #box_num = -2 #-2, -1, 0, 1
+	dataset1 = create_box_dataset(1, gpm_transformed_data, region_center_y_idy, projvec, files_gpm, number_of_pixels)
+	datasetm2 = create_box_dataset(-2, gpm_transformed_data, region_center_y_idy, projvec, files_gpm, number_of_pixels)
+
+		
+	return([dataset1,datasetm2])
 	
 	
 	
--- gpm_downloads.py
+++ gpm_downloads.py
@@ -19,6 +19,7 @@ start_timing = time.time()
 
 # GOES channels to download
 channels = [8,13] #[8, 13]
+number_of_pixels = 256#512
 
 # Paths
 linkfile = "link_file_test.txt" #"2017-12-18--2018-12-17-with-shapefile.txt" 
@@ -50,128 +51,116 @@ link_list = link_file.readlines()
 link_file.close()
 
 
-for i in range(0,2,8): 
-	
-	# ----------------------------- FOR EACH LINK -----------------------------
-	
-	print(i)
-	gpm_file_t_0, gpm_file_t_1 = gpm_extract_datetime(link_list[i])
-	print('GPM time: ' + str(gpm_file_t_0) + ', ' + str(gpm_file_t_1))
+i=0
+
+# ----------------------------- FOR EACH LINK -----------------------------
+
+gpm_file_time = gpm_extract_datetime(link_list[i])
 
 
-	# DOWNLOAD gpm data
-	files_gpm = l2b_gpm_cmb.download(gpm_file_t_0, gpm_file_t_1, path_to_download_storage_gpm)
-	dataset_gpm = l2b_gpm_cmb.open(files_gpm[0])
+print(gpm_data_processing(number_of_pixels, gpm_file_time, path_to_download_storage_gpm, area_def, region_corners))
 
-	# TEST
-	#dataset_gpm = l2b_gpm_cmb.open("GPM/2B.GPM.DPRGMI.2HCSHv4-1.20180314-S194719-E211954.022965.V06A.HDF5") #TEST
 
-	# Arrival and departure times
-	in_brazil_t_0, in_brazil_t_1 = extract_time_in_brazil(latlon_limits,
-							 	dataset_gpm)
-							 	
-	print('In Brazil: ' + str(in_brazil_t_0) + ', ' + str(in_brazil_t_1))
+#TODO: Goes downloading with cache
+'''
+
+ 
+
+# DOWNLOAD goes data
+files_goes = []
+files_tmp = GOES16L1BRadiances("F", channels[0]).download(in_brazil_t_0, in_brazil_t_1, path_to_download_storage_goes)
+goes_index = 0
+	
+if (len(files_tmp) > 1):
+	time_list_goes = [GOES16L1BRadiances("F",
+	channels[0]).filename_to_date(file_tmp) for file_tmp in files_tmp]
+	mean_time_brazil = in_brazil_t_0 + datetime.timedelta(
+	seconds = (in_brazil_t_1 - in_brazil_t_0).seconds/2)
+	timediff = [np.abs((time_goes-mean_time_brazil).seconds) 
+	for time_goes in time_list_goes]				
+	goes_index = np.argmin(timediff)
+elif (len(files_tmp) == 0):
+	continue    
+
+files_goes.append(files_tmp[goes_index])
 	
 	
-	# DOWNLOAD goes data
-	files_goes = []
-	files_tmp = GOES16L1BRadiances("F", channels[0]).download(in_brazil_t_0, in_brazil_t_1, path_to_download_storage_goes)
-	goes_index = 0
-		
-	if (len(files_tmp) > 1):
-		time_list_goes = [GOES16L1BRadiances("F",
-		channels[0]).filename_to_date(file_tmp) for file_tmp in files_tmp]
-		mean_time_brazil = in_brazil_t_0 + datetime.timedelta(
-		seconds = (in_brazil_t_1 - in_brazil_t_0).seconds/2)
-		timediff = [np.abs((time_goes-mean_time_brazil).seconds) 
-		for time_goes in time_list_goes]				
-		goes_index = np.argmin(timediff)
-	elif (len(files_tmp) == 0):
-		continue    
-	
+for channel in channels[1:]:
+	files_tmp = GOES16L1BRadiances("F", channel).download(in_brazil_t_0, 
+	in_brazil_t_1, path_to_download_storage_goes)
 	files_goes.append(files_tmp[goes_index])
-		
-		
-	for channel in channels[1:]:
-		files_tmp = GOES16L1BRadiances("F", channel).download(in_brazil_t_0, 
-		in_brazil_t_1, path_to_download_storage_goes)
-		files_goes.append(files_tmp[goes_index])
-		
-	print('goes files downloaded')	
 	
-	# TEST
-	#files_08 = [Path('GOES-16/OR_ABI-L1b-RadF-M3C08_G16_s20180732000427_e20180732011193_c20180732011242.nc')] #TEST
-	#files_13 = [Path('GOES-16/OR_ABI-L1b-RadF-M3C13_G16_s20180732000427_e20180732011205_c20180732011259.nc')] #TEST
-	#files_goes = [files_08[0], files_13[0]] #TEST
+print('goes files downloaded')	
 
-	# Resample coordinates for GPM data into GOES data format
-	swath_def = geometry.SwathDefinition(lons=dataset_gpm['longitude'], lats=dataset_gpm['latitude'])
-	gpm_transformed_data = kd_tree.resample_nearest(swath_def, dataset_gpm['surface_precipitation'].values, 
-	area_def, radius_of_influence=50000, epsilon=0.5, fill_value=None)
-	print('gpm transformed')
-	
+# TEST
+#files_08 = [Path('GOES-16/OR_ABI-L1b-RadF-M3C08_G16_s20180732000427_e20180732011193_c20180732011242.nc')] #TEST
+#files_13 = [Path('GOES-16/OR_ABI-L1b-RadF-M3C13_G16_s20180732000427_e20180732011205_c20180732011259.nc')] #TEST
+#files_goes = [files_08[0], files_13[0]] #TEST
 
-	files_goes = map(str, files_goes)
-	goes_scn = Scene(reader='abi_l1b', filenames=files_goes)
-	av_dat_names = goes_scn.available_dataset_names()
+files_goes = map(str, files_goes)
+goes_scn = Scene(reader='abi_l1b', filenames=files_goes)
+av_dat_names = goes_scn.available_dataset_names()
 
-	# This is a warning regarding loss of projection information when converting to a PROJ string
-	with warnings.catch_warnings():
-		warnings.simplefilter('ignore')
-		goes_scn.load((av_dat_names))
-	
-	print('Channels: ' +str(av_dat_names))
-	
-	# This is a warning regarding loss of projection information when converting to a PROJ string
-	with warnings.catch_warnings():
-		warnings.simplefilter('ignore')	
-		goes_scn = goes_scn.resample(goes_scn.min_area(), resampler = 'native')
-		
-	ref_height = goes_scn['C'+str(channels[0]).zfill(2)].y.shape[0]
-	ref_width = goes_scn['C'+str(channels[0]).zfill(2)].x.shape[0]
-	
-	goes_scn = goes_scn.aggregate(x=np.int(ref_width/width), 
-	y=np.int(ref_height/height), func='mean')
-	
-	# RuntimeWarning: invalid value encountered in true_divide x = np.divide(x1, x2, out)
-	# Caused by doing mean of nan-values in aggregate
-	with warnings.catch_warnings():
-		warnings.simplefilter('ignore')
-		# CREATE combined dataset (full disk)
-		keys = ['C'+str(i).zfill(2) for i in channels]
-		values = [(["y", "x"], goes_scn['C' +str(i).zfill(2)].values) for i in channels]
-		keys.append('gpm_precipitation')
-		values.append((["y", "x"], gpm_transformed_data))
-		data_vars_dict = dict(zip(keys, values))
+# This is a warning regarding loss of projection information when converting to a PROJ string
+with warnings.catch_warnings():
+	warnings.simplefilter('ignore')
+	goes_scn.load((av_dat_names))
 
-		dataset = xr.Dataset(
-			coords = dict(
-				y = (["y"], goes_scn['C'+str(channels[0]).zfill(2)].y), 
-				x = (["x"], goes_scn['C'+str(channels[0]).zfill(2)].x)),
-			data_vars = data_vars_dict, 
-			attrs = dict(
-				time_goes = str({'start_time': goes_scn['C'+str(channels[0]).zfill(2)].attrs['start_time'],
-					'end_time': goes_scn['C'+str(channels[0]).zfill(2)].attrs['end_time']}), 
-				time_gpm = str({'start_time': gpm_file_t_0, 'end_time': gpm_file_t_1, 
-					'approx_start_time_region': in_brazil_t_0, 'approx_end_time_region': in_brazil_t_1})
-					))
-	print('dataset created')
-	
-	# CROPPING dataset to interesting region
-	mask_x = (dataset.x > region_corners[0])*(dataset.x < region_corners[2])
-	mask_y = (dataset.y > region_corners[1])*(dataset.y < region_corners[3])
-	sliced_dataset = dataset.where(mask_x & mask_y, drop = True)
+print('Channels: ' +str(av_dat_names))
 
-	print('dataset cropped')
+# This is a warning regarding loss of projection information when converting to a PROJ string
+with warnings.catch_warnings():
+	warnings.simplefilter('ignore')	
+	goes_scn = goes_scn.resample(goes_scn.min_area(), resampler = 'native')
+	
+ref_height = goes_scn['C'+str(channels[0]).zfill(2)].y.shape[0]
+ref_width = goes_scn['C'+str(channels[0]).zfill(2)].x.shape[0]
 
-	# Writing dataset to file
-	filename = path_to_store_processed_data+'/datasetS'+str(gpm_file_t_0).replace(" ", "")+'E'+str(gpm_file_t_1).replace(" ", "")+str(channels)+'created'+str(datetime.now()).replace(" ", "")+'.nc'
-	sliced_dataset.to_netcdf(filename)
-	
-	print('dataset saved')
-	
-	# -------------------------------------------------------------------------
-	
+goes_scn = goes_scn.aggregate(x=np.int(ref_width/width), 
+y=np.int(ref_height/height), func='mean')
+
+
+# RuntimeWarning: invalid value encountered in true_divide x = np.divide(x1, x2, out)
+# Caused by doing mean of nan-values in aggregate
+with warnings.catch_warnings():
+	warnings.simplefilter('ignore')
+	# CREATE combined dataset (full disk)
+	keys = ['C'+str(i).zfill(2) for i in channels]
+	values = [(["y", "x"], goes_scn['C' +str(i).zfill(2)].values) for i in channels]
+	keys.append('gpm_precipitation')
+	values.append((["y", "x"], gpm_transformed_data))
+	data_vars_dict = dict(zip(keys, values))
+
+	dataset = xr.Dataset(
+		coords = dict(
+			y = (["y"], goes_scn['C'+str(channels[0]).zfill(2)].y), 
+			x = (["x"], goes_scn['C'+str(channels[0]).zfill(2)].x)),
+		data_vars = data_vars_dict, 
+		attrs = dict(
+			time_goes = str({'start_time': goes_scn['C'+str(channels[0]).zfill(2)].attrs['start_time'],
+				'end_time': goes_scn['C'+str(channels[0]).zfill(2)].attrs['end_time']}), 
+			time_gpm = str({'start_time': gpm_file_t_0, 'end_time': gpm_file_t_1, 
+				'approx_start_time_region': in_brazil_t_0, 'approx_end_time_region': in_brazil_t_1})
+				))
+print('dataset created')
+
+# CROPPING dataset to interesting region
+mask_x = (dataset.x > region_corners[0])*(dataset.x < region_corners[2])
+mask_y = (dataset.y > region_corners[1])*(dataset.y < region_corners[3])
+sliced_dataset = dataset.where(mask_x & mask_y, drop = True)
+
+print('dataset cropped')
+
+# Writing dataset to file
+filename = path_to_store_processed_data+'/datasetS'+str(gpm_file_t_0).replace(" ", "t")+'E'+str(gpm_file_t_1).replace(" ", "t")+str(channels).replace(" ", "")+'created'+str(datetime.now()).replace(" ", "")+'.nc'
+sliced_dataset.to_netcdf(filename)
+
+print('dataset saved')
+
+'''
+
+# -------------------------------------------------------------------------
+
 end_timing = time.time()
 total_timing = end_timing-start_timing
 print('total time: ' + str(total_timing)+ ' seconds')
