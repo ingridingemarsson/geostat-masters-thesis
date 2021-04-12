e9bebfe40e19f70d29be616e9fb3e2814fb507e9
Ingrid Ingemarsson <inina@hotmail.se>
2021-04-12 14:01:34 +0200

    changes to run testjob, now working

--- src/test_run.py
+++ src/test_run.py
@@ -1,6 +1,6 @@
 import numpy as np
 import argparse
-
+import os
 
 parser = argparse.ArgumentParser(description='Train fully-connected QRNN')
 parser.add_argument(
@@ -23,8 +23,8 @@ path_to_data = args.path_to_data
 path_to_storage = args.path_to_storage
 
 
-X_train = np.load(path_to_data+'train/X_singles_dataset.npy')
+X_train = np.load(os.path.join(path_to_data,'train/X_singles_dataset.npy'))
 
 res = X_train[0]
 
-np.save(path_to_storage + 'test.npy', res)
+np.save(os.path.join(path_to_storage, 'test.npy'), res)
