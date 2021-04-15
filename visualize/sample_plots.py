5a61c6748d082c7995191f6ef96f50a8962258ee
Ingrid Ingemarsson <inina@hotmail.com>
2021-04-15 17:10:15 +0200

    update of visualization

--- visualize/sample_plots.py
+++ visualize/sample_plots.py
@@ -4,10 +4,10 @@ from matplotlib.colors import LogNorm
 from matplotlib.gridspec import GridSpec
 import torch
 
-def plotRandomSample(dataset, qrnn = None, device='cpu', c=[0,4], uselog=False):
-    ncols = 3
+def plotRandomSample(dataset, channels, qrnn = None, device='cpu', cha=[0,4], uselog=False):
+    ncols = len(cha)+1
     if not qrnn == None:
-        ncols = 4
+        ncols += 1
     
     f = plt.figure(figsize=(15, 6))
     gs =  GridSpec(2, ncols, figure=f, height_ratios=[1.0, 0.1])
@@ -15,21 +15,17 @@ def plotRandomSample(dataset, qrnn = None, device='cpu', c=[0,4], uselog=False):
     data = dataset[index]
     precip_norm = LogNorm(1e-2, 1e2)
 
-    ax = plt.subplot(gs[0, 0])
-    m = ax.imshow(data['box'].numpy()[c[0]], cmap=plt.get_cmap('inferno'))
-    ax.grid(False)
-    ax.set_title("(a) channel c[0]", loc="left")
-    ax = plt.subplot(gs[1, 0])
-    plt.colorbar(m, cax=ax, orientation="horizontal", label="Normalized brightness temperature")
+    i = 0
+    for c in cha:
+	    ax = plt.subplot(gs[0, i])
+	    m = ax.imshow(data['box'].numpy()[channels.index(c)], cmap=plt.get_cmap('inferno'))
+	    ax.grid(False)
+	    ax.set_title("(a) channel "+str(c), loc="left")
+	    ax = plt.subplot(gs[1, i])
+	    plt.colorbar(m, cax=ax, orientation="horizontal", label="Normalized brightness temperature")
+	    i+=1
 
-    ax = plt.subplot(gs[0, 1])
-    m = ax.imshow(data['box'].numpy()[c[1]], cmap=plt.get_cmap('inferno'))
-    ax.grid(False)
-    ax.set_title("(b) channel c[1]", loc="left")
-    ax = plt.subplot(gs[1, 1])
-    plt.colorbar(m, cax=ax, orientation="horizontal", label="Normalized brightness temperature")
-
-    ax = plt.subplot(gs[0, 2])
+    ax = plt.subplot(gs[0, len(cha)])
     y_true = data['label'].numpy()
     if uselog:
     	y_true = np.where(y_true != -1, np.exp(y_true), y_true) 
@@ -37,19 +33,19 @@ def plotRandomSample(dataset, qrnn = None, device='cpu', c=[0,4], uselog=False):
     ax.imshow(y_true!=-1,  cmap=plt.get_cmap('binary_r'), alpha = 0.02)
     ax.grid(False)
     ax.set_title("(c) Reference rain rate", loc="left")
-    ax = plt.subplot(gs[1, 2])
+    ax = plt.subplot(gs[1, len(cha)])
     plt.colorbar(m, cax=ax, orientation="horizontal", label=r"Rain rate [mm/h]")
 
     if not qrnn == None:
         y_pred = torch.squeeze(qrnn.posterior_mean(x=data['box'].unsqueeze(0).to(device))).cpu().detach().numpy()
         if uselog:
             y_pred = np.where(y_pred != -1, np.exp(y_pred), y_pred) 
-        ax = plt.subplot(gs[0, 3])
+        ax = plt.subplot(gs[0, len(cha)+1])
         m = ax.imshow(y_pred, norm=precip_norm, cmap=plt.get_cmap('GnBu'))
         ax.imshow(y_true!=-1,  cmap=plt.get_cmap('binary_r'), alpha = 0.02)
         ax.grid(False)
         ax.set_title("(d) Model prediction rain rate", loc="left")
-        ax = plt.subplot(gs[1, 3])
+        ax = plt.subplot(gs[1, len(cha)+1])
         plt.colorbar(m, cax=ax, orientation="horizontal", label=r"Rain rate [mm/h]")
 
 
