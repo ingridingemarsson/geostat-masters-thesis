found and corrected masking, now excluding negative values in precip data

dataset/data/extract_singles_dataset.py
@@ -23,7 +23,7 @@ def ConvertDatasetToSingles(channels, path_to_data_list, path_to_storage, max_nu
                     box_dataset = xr.open_dataset(os.path.join(path,file))
                     box_dataset.close()
 
-                    label_exists = (np.isnan(box_dataset['gpm_precipitation'].values)==False)
+                    label_exists = np.logical_and((np.isnan(box_dataset['gpm_precipitation'].values)==False),(box_dataset['gpm_precipitation'].values >= 0.0))
                     idy, idx = np.where(label_exists)
                     labs = box_dataset['gpm_precipitation'].values[idy,idx]
                     vals = np.zeros((np.sum(label_exists),len(channels)))

src/load_data.py
@@ -43,8 +43,9 @@ class Mask(object):
 		
 		filled_box = np.stack([np.where(np.isnan(box[i]), self.fillvalue, box[i]) for i in range(box.shape[0])])
 		
-		filled_label = np.where(np.isnan(label), self.fillvalue, label)
-
+		#filled_label = np.where(np.isnan(label), self.fillvalue, label)
+		filled_label = np.where(np.logical_or(np.isnan(label), label<0.0), self.fillvalue, label)
+		
 		return {'box': filled_box, 'label': filled_label}
 		
 